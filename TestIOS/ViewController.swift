e0e7fa35ec2eb34e08b83eac0e405a6052f7c290
n.baklanov <n.baklanov@vk.team>
2025-10-16 09:48:49 +0600

    First commit

---
+++ TestIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestIOS
+//
+//  Created by Baklanov Nikolay on 16.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
